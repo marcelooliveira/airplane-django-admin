8060683325de1e3bee5bfa50e8ae230875275ed7
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-04-02 15:14:48 -0300

    migrations

--- KitchenShop/KitchenShop/custom/models.py
+++ KitchenShop/KitchenShop/custom/models.py
@@ -6,7 +6,25 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = "Products"
 
+    def __str__(self):
+        return f"{self.description}"
+
 class Inventory(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.OneToOneField(
+        Product,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )    
     quantity = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(1000000)])
     unit_price = models.DecimalField(max_digits=12, decimal_places=2)
+
+    class Meta:
+        verbose_name_plural = "Inventory"
+
+    def __str__(self):
+        product_description = Product.objects.filter(id=self.product.id)[0].description
+        return f"{product_description}, qty: {self.quantity}, unit price: ${self.unit_price:2}"
+
+    # def __str__(self):
+    #     product_description = Product.objects.filter(id=self.product).first().description
+    #     return f"{product_description} - qty: {self.quantity}, unit price: ${self.unit_price:2} "
